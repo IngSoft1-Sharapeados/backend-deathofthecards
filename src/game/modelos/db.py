eb797756dd62ba0a1b160967ce6e0c17dee59849
Fran <f.canete@mi.unc.edu.ar>
2025-09-23 17:38:48 -0300

    SCRUM-7 quick fix crear partida

--- src/game/modelos/db.py
+++ src/game/modelos/db.py
@@ -2,14 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from settings import settings
 
-
 def get_engine():
     return create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 
 def get_session_local(engine):
     return sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
+
 Base = declarative_base()
+
+from game.partidas.models import Partida
+from game.jugadores.models import Jugador
+from game.cartas.models import Carta
 
 #Dependencia
 def get_db():
