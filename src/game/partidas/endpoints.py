b5cb0629cf3f6598de69a74b163da0a01cb73d42
Mikhail <mikhail.wysoki@mi.unc.edu.ar>
2025-09-23 19:48:56 -0300

    se implementa el endpoint de listar partidas

--- src/game/partidas/endpoints.py
+++ src/game/partidas/endpoints.py
@@ -2,7 +2,7 @@ from typing import List, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from game.partidas.models import Partida
-from game.partidas.schemas import PartidaData, PartidaResponse
+from game.partidas.schemas import PartidaData, PartidaResponse, PartidaListar
 from game.partidas.services import PartidaService
 from game.modelos.db import get_db
 
@@ -46,6 +46,34 @@ async def crear_partida(partida_info: PartidaData, db=Depends(get_db)
             )
         return PartidaResponse(id_partida=partida_creada.id)
 
+@partidas_router.get(path="", status_code = status.HTTP_200_OK)
+async def listar_partidas(db=Depends(get_db)) -> List[PartidaListar]:
+
+    """
+    Lista las partidas no iniciadas de la base de datos.
+    
+    Returns
+    -------
+    List[PartidaListar]
+        Respuesta con la lista de las partidas.
+    """
+
+    partidas_listadas = PartidaService(db).listar()
+    if not partidas_listadas:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No hay partidas disponibles"
+        )
+    else:
+        return [
+            PartidaListar(
+                id=p.id,
+                nombre=p.nombre,
+                iniciada=p.iniciada,
+                maxJugadores=p.maxJugadores
+            )
+            for p in partidas_listadas
+        ]
 
 # endpoint post /partidas crear (devuelve id_partida) faltan unittest
 # endpoint get /partidas listar (devuelve lista de partidas con nombre partida, cantJugadores, lista jugadores)
